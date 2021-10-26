83f59d4010e4a6d0fc255c6ad94e4b1c8a9b72d8
Fabio Giulio Tonolo <38096632+fabiogt@users.noreply.github.com>
2021-10-26 16:21:42 +0200

    style

---
+++ data.js
@@ -0,0 +1,594 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-piano-terra---centrale",
+      "name": "Piano terra - Centrale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.9984250762416345,
+        "pitch": -0.1772747884054855,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.001967845521367,
+          "pitch": -0.30847385752603174,
+          "rotation": 0,
+          "target": "1-pianerottolo-0-1-centrale"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0209553169343435,
+          "pitch": -0.044349155039267174,
+          "title": "Istituto Bosso-Monti",
+          "text": "Scalone centrale"
+        }
+      ]
+    },
+    {
+      "id": "1-pianerottolo-0-1-centrale",
+      "name": "Pianerottolo 0-1 centrale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.0386999760222313,
+        "pitch": 0.41705928380271295,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.706862777318607,
+          "pitch": -0.25066045578843976,
+          "rotation": 0,
+          "target": "2-pianerottolo-0-1-destra"
+        },
+        {
+          "yaw": -1.959250981024402,
+          "pitch": 0.4360780748671047,
+          "rotation": 0,
+          "target": "0-piano-terra---centrale"
+        },
+        {
+          "yaw": -0.39870706850487814,
+          "pitch": -0.28930071306827543,
+          "rotation": 0,
+          "target": "3-pianerottolo-0-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pianerottolo-0-1-destra",
+      "name": "Pianerottolo 0-1 destra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.3700504420292887,
+        "pitch": 0.16139036400445939,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.675664324292816,
+          "pitch": 0.3857652620238472,
+          "rotation": 0,
+          "target": "1-pianerottolo-0-1-centrale"
+        },
+        {
+          "yaw": 2.9988942614481875,
+          "pitch": -0.3068530295066374,
+          "rotation": 0,
+          "target": "4-piano-1-destra"
+        },
+        {
+          "yaw": -1.725013641965738,
+          "pitch": 0.02676598218014803,
+          "rotation": 0,
+          "target": "3-pianerottolo-0-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pianerottolo-0-1-sinistra",
+      "name": "Pianerottolo 0-1 sinistra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.6329042766220105,
+        "pitch": 0.038350185506008216,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.507983376831884,
+          "pitch": 0.4277542065240816,
+          "rotation": 0,
+          "target": "1-pianerottolo-0-1-centrale"
+        },
+        {
+          "yaw": -2.51636377097061,
+          "pitch": 0.045931837241164786,
+          "rotation": 0,
+          "target": "2-pianerottolo-0-1-destra"
+        },
+        {
+          "yaw": -0.8393378432025411,
+          "pitch": -0.3261514213495591,
+          "rotation": 0,
+          "target": "6-piano-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-piano-1-destra",
+      "name": "Piano 1 destra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.6792659582346676,
+        "pitch": 0.023968865941256468,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.136994398959196,
+          "pitch": 0.3530604408156748,
+          "rotation": 0,
+          "target": "2-pianerottolo-0-1-destra"
+        },
+        {
+          "yaw": 2.595586793080285,
+          "pitch": 0.168076576377155,
+          "rotation": 0,
+          "target": "5-piano-1-centrale"
+        },
+        {
+          "yaw": 2.5560323811953456,
+          "pitch": 0.07978455096178472,
+          "rotation": 0,
+          "target": "6-piano-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-piano-1-centrale",
+      "name": "Piano 1 centrale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0874180370844613,
+        "pitch": -0.14632209562323695,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.55941810503462,
+          "pitch": 0.10472997695039155,
+          "rotation": 0,
+          "target": "4-piano-1-destra"
+        },
+        {
+          "yaw": -3.1393919509981956,
+          "pitch": -0.28629603273520843,
+          "rotation": 0,
+          "target": "7-pianerottolo-1-2-centrale"
+        },
+        {
+          "yaw": 1.6365169283083496,
+          "pitch": 0.10670691528300935,
+          "rotation": 0,
+          "target": "6-piano-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-piano-1-sinistra",
+      "name": "Piano 1 sinistra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.4081005622666236,
+        "pitch": -0.007658641495638108,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.366172804816312,
+          "pitch": 0.10962878440243884,
+          "rotation": 0,
+          "target": "5-piano-1-centrale"
+        },
+        {
+          "yaw": -1.3045155855102095,
+          "pitch": 0.019773016507773633,
+          "rotation": 0,
+          "target": "4-piano-1-destra"
+        },
+        {
+          "yaw": -2.8277644558859922,
+          "pitch": 0.31562625167030056,
+          "rotation": 0,
+          "target": "3-pianerottolo-0-1-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pianerottolo-1-2-centrale",
+      "name": "Pianerottolo 1-2 centrale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.5108073111443368,
+        "pitch": 0.17708897018118464,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.462872397901629,
+          "pitch": 0.3994522806920138,
+          "rotation": 0,
+          "target": "5-piano-1-centrale"
+        },
+        {
+          "yaw": -3.074736636146662,
+          "pitch": -0.27197111542751173,
+          "rotation": 0,
+          "target": "8-pianerottolo-1-2-destra"
+        },
+        {
+          "yaw": 0.05832243867189746,
+          "pitch": -0.2821688788328096,
+          "rotation": 0,
+          "target": "9-pianerottolo-1-2-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pianerottolo-1-2-destra",
+      "name": "Pianerottolo 1-2 destra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.061858831839327,
+        "pitch": 0.21893464468016788,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5063016661163893,
+          "pitch": 0.39164951051368035,
+          "rotation": 0,
+          "target": "7-pianerottolo-1-2-centrale"
+        },
+        {
+          "yaw": -3.114455667749887,
+          "pitch": -0.2951657355397028,
+          "rotation": 0,
+          "target": "10-piano-2-destra"
+        },
+        {
+          "yaw": -1.5603307282130494,
+          "pitch": 0.018873990167078247,
+          "rotation": 0,
+          "target": "9-pianerottolo-1-2-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pianerottolo-1-2-sinistra",
+      "name": "Pianerottolo 1-2 sinistra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.1205521741543372,
+        "pitch": 0.06871254807697724,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4281710252013955,
+          "pitch": -0.3241753520768498,
+          "rotation": 0,
+          "target": "11-piano-2-sinistra"
+        },
+        {
+          "yaw": -1.98731368535846,
+          "pitch": 0.41879177774447207,
+          "rotation": 0,
+          "target": "5-piano-1-centrale"
+        },
+        {
+          "yaw": -2.0109668721532046,
+          "pitch": 0.04326749458388868,
+          "rotation": 0,
+          "target": "8-pianerottolo-1-2-destra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-piano-2-destra",
+      "name": "Piano 2 destra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.445739279595969,
+        "pitch": -0.02213441141760697,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7356971063954933,
+          "pitch": 0.25101180210788243,
+          "rotation": 0,
+          "target": "8-pianerottolo-1-2-destra"
+        },
+        {
+          "yaw": 2.960252642387749,
+          "pitch": 0.07717130403512229,
+          "rotation": 0,
+          "target": "11-piano-2-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-piano-2-sinistra",
+      "name": "Piano 2 sinistra",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.6583280289975786,
+        "pitch": 0.05366434691956101,
+        "fov": 1.3349517233366994
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8672714182464638,
+          "pitch": 0.06169288008821816,
+          "rotation": 0,
+          "target": "10-piano-2-destra"
+        },
+        {
+          "yaw": 2.823810804880054,
+          "pitch": 0.331569844282388,
+          "rotation": 0,
+          "target": "9-pianerottolo-1-2-sinistra"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Istituto Bosso-Monti: Scalone centrale",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
